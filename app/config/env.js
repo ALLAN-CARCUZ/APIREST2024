473af2c269d91eaa726cc915d52eabe42ab8f217
ALLAN-CARCUZ <99932347+ALLAN-CARCUZ@users.noreply.github.com>
2024-08-14 23:18:15 -0600

    first commit

---
+++ app/config/env.js
@@ -0,0 +1,17 @@
+
+
+const env = {
+  database: 'umg_antigua_web445',
+  username: 'umg_antigua_web445_user',
+  password: 'JEsrnTDYFlmlqqg5xP4ZoAMI5YtFBo5b',
+  host: 'dpg-cqc7lot6l47c73cv9hc0-a.oregon-postgres.render.com',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;
