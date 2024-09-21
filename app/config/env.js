1e36cb4b0c9303b5d957833dfa64285f9d0cebfd
ALLAN-CARCUZ <99932347+ALLAN-CARCUZ@users.noreply.github.com>
2024-09-21 16:33:16 -0600

    NuevaBD

--- app/config/env.js
+++ app/config/env.js
@@ -1,10 +1,10 @@
 
 
 const env = {
-  database: 'umg_antigua_web445',
-  username: 'umg_antigua_web445_user',
-  password: 'JEsrnTDYFlmlqqg5xP4ZoAMI5YtFBo5b',
-  host: 'dpg-cqc7lot6l47c73cv9hc0-a.oregon-postgres.render.com',
+  database: 'umg_antigua_web445_ef83',
+  username: 'umg_antigua_web445_ef83_user',
+  password: 'vD9Yn8jGAtqS0yFj6wxEVXYltUtVWzsi',
+  host: 'dpg-crnjee08fa8c738hf1qg-a.oregon-postgres.render.com',
   dialect: 'postgres',
   pool: {
     max: 5,
